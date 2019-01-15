218350f5bc3fedc6797f7528f29ebb60f4943413
mattdioso <dioso.matt@gmail.com>
2019-01-14 19:55:28 -0800

    Added Spotify authorization

---
+++ oauth.js
@@ -0,0 +1,97 @@
+function startSpotifyOAuth(success_function) {
+	var client_id = 'ecbbd1a3f1b148019e8f7454e0f89205';
+	var client_secret = '409ef0fc3a4743b89c3d9a2e35d59f39';
+	var redirectUri = chrome.identity.getRedirectURL("spotify");
+	var success_function_one = success_function;
+
+	chrome.identity.launchWebAuthFlow({
+		"url":"https://accounts.spotify.com/authorize?client_id=" + client_id +
+		"&redirect_uri=" + encodeURIComponent(redirectUri) + 
+		"&response_type=code&scope=app-remote-control%20playlist-read-private%20play-list-modify-private%20playlist-modify-public%20user-library-read%20playlist-modify%20user-modify-playback-state",
+		'interactive': true,
+	},
+		function(redirect_url) {
+			var parsedParameters = {}
+			var queryString = redirect_url.split("?")[1];
+			var queryParameters = queryString.split("&");
+			var success_function_two = success_function_one;
+			for (var parameterNumber in queryParameters) {
+				var parameter = queryParameter[parameterNumber];
+				var name = parameter.split("=")[0];
+				var value = decodeURIComponent(parameter.split("=")[1]);
+				parsedParameters[name] = value;
+			}
+			var code = parsedParameters['code'];
+			var req = new XMLHttpRequest();
+			req.open("POST", "https://accounts.spotify.com/api/token", true);
+			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			req.setRequestHeader("Authorization", "Basic " + btoa(client_id + ":" + client_secret));	
+			req.onreadystatechange = function() {
+				if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+					var value = JSON.parse(this.response);
+					value['start_time'] = new Date().getTime()/1000;
+					chrome.storage.local.set({'spotify_auth':value}, function() {
+						success_function();
+					});
+				}
+			}
+			req.send("grant_type=authorization_code&code=" + encodeURIComponent(code) + "&redirect_uri=" + encodeURIComponent(redirectUri));
+		});
+}
+
+function startOauth() {
+	var client_id = 'ecbbd1a3f1b148019e8f7454e0f89205';
+	var redirectUri = chrome.identity.getRedirectURL() + "spotify";
+
+	chrome.identity.launchWebAuthFlow({
+		"url":"https://accounts.spotify.com/authorize?client_id=" + client_id + 
+		"&redirect_uri=" + encodeURIComponent(redirectUri) + 
+		"&response_type=token",
+		'interactive':true,
+	},
+		function(redirect_url) {
+		});
+}
+
+function refresh_token(spotify_auth, function_to_call) {
+	var client_id = 'ecbbd1a3f1b148019e8f7454e0f89205';
+	var client_secret = '409ef0fc3a4743b89c3d9a2e35d59f39';
+
+	var req = new XMLHttpRequest();
+	req.open("POST", "https://accounts.spotify.com/api/token", true);
+	req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	req.setRequestHeader("Authorization", "Basic " + btoa(client_id + ":" + client_secret));
+	req.onreadystatechange = function() {
+		if (this.readyState == XMLHttpRequest.DONE && this.status ==200) {
+			var value = JSON.parse(this.response);
+			var internal_function = function_to_call;
+			spotify_auth['access_token'] = value['access_token']
+			spotify_auth['start_time'] = new Date().getTime() / 1000;
+			chrome.storage.local.set({'spotify_auth':spotify_auth}, function() {
+				internal_function(spotify['access_token']);
+			});
+		}
+	}
+	req.send("grant_type=refresh_token&refresh_token=" + encodeURIComponent(spotify_auth['refresh_token']));
+
+}
+
+function fetchSpotifyAuthorizationToken(function_to_call) {
+	var call_back_function = function_to_call;
+	chrome.storage.local.get(['spotify_auth'], function(value) {
+		if (value['spotify_auth']){
+			var internal_function = function_to_call;
+			var spotify_auth = value['spotify_auth'];
+			var current_time = new Date().getTime()/1000;
+			if (current_time > spotify_auth['start_time'] + spotify_auth['expires_in']) {
+				refresh_token(spotify_auth, internal_function);
+			} else {
+				internal_function(spotify_auth['access_token']);
+			}
+		} else {
+			startSpotifyOAuth(function() {
+				fetchSpotifyAuthorizationToken(call_back_function);
+			});
+		}
+	});
+}
